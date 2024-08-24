8959f94f9d1f3dc62872eef87a9a15f39a41e015
Sagar Prasad <sagarprasad@Sagars-MacBook-Air.local>
2024-08-24 15:16:06 +0530

    feat: initial commit

---
+++ src/main/java/com/example/spring_ai_nvidia_llm/SpringAiNvidiaLlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_nvidia_llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiNvidiaLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiNvidiaLlmApplication.class, args);
+	}
+
+}
